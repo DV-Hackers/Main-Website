217beeb1aebf807de94c12d41d14547ad4fe0a6d
jacobhanson <jhanreg11@gmail.com>
2019-09-14 15:07:57 -0700

    positioned elements

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import './css/home.css';
+
 import Typed from 'react-typed'
 import Timer from './components/timer'
-import { Jumbotron, Container, Navbar, NavbarBrand, Nav, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import { Jumbotron, Container, Navbar, NavbarBrand, Nav, NavbarToggler, NavItem, NavLink, Fade, Col } from 'reactstrap';
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
   render () {
     return (
         <Jumbotron fluid>
           <Navbar expand='md' fixed='top' dark>
-            <NavbarBrand href='/home/'><img src='./img/logo.png' alt='DVHackers'/></NavbarBrand>
-            <Nav className='ml-auto' navbar>
+            <NavbarBrand href='/home/'><img id='navbrand' src={require('./img/logo.png')} alt='DVHackers'/></NavbarBrand>
+            <Nav id='nav-items' className='ml-auto' navbar>
               <NavItem><NavLink href='/home/'>Home</NavLink></NavItem>
               <NavItem><NavLink href='/about/'>About</NavLink></NavItem>
               <NavItem><NavLink href='/team/'>Team</NavLink></NavItem>
@@ -18,17 +23,23 @@ class Home extends React.Component {
             </Nav>
           </Navbar>
 
-        <div id='center-wrapper'>
+        <Col id='center-wrapper' sm="12" md={{ size: 6, offset: 3 }}>
           <Typed id='landing-typed' strings={['DVHackers']} typeSpeed={200}></Typed>
-          <div id='tag-line'>Talk is cheap. Show us the code</div>
-        </div>
+          <Fade id='tagline' in={true} timeout={3500}>Talk is cheap, show us the code.</Fade>
+        </Col>
 
-        <div id='timer-wrapper'>
-          <div id='timer-text'>Time until next meeting: </div>
-          <Timer date='September 16, 2019 18:30:00 PST' delay='90'/>
-        </div>
-        </Jumbotron>
+        <Fade in={true} timeout={3500}>
+          <Col id='timer-wrapper' sm={{ size: 'auto' }}>
+            <div id='timer-text'>Time until next meeting:&nbsp;</div>
+            <Timer date='September 16, 2019 18:30:00 PST' delay='90'/>
+          </Col>
+        </Fade>
+      </Jumbotron>
     );
+  }
+
+  componentDidMount() {
+    setTimeout(this.fadeIn, 3000)
   }
 }
 
