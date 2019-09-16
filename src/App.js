0319f840e5e78bfe79c68a1e4eaa2b43e5846eee
Andrew Che <andrew.che0122@gmail.com>
2019-09-16 02:02:53 -0700

    Fixed CSS & CircleCI config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/home.js';
+import About from './pages/about.js';
+import { Container, Row, Col } from 'reactstrap';
 
 function App() {
   return (
     <>
       <Home />
+      <About />      
     </>
   );
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,1 +1,20 @@
 import React from 'react';
+import './css/about.css'
+
+import { Jumbotron } from 'reactstrap'
+
+class About extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return (
+            <div className="background">
+                About Us
+            </div>
+        );
+    }
+}
+
+export default About;
