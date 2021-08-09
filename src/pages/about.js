fc30f106a1fddd5cbcd25c897068731dff1cd968
Yahya <54677127+yahya-s9@users.noreply.github.com>
2021-08-09 15:26:30 -0700

    Update about.js
    Updating for 2021-2022

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,8 +13,8 @@ class About extends React.Component {
             <div className="section" id='about'>
                 <div className="title">About Us</div>
                 <div id="body">
-                    <div id="description">DVHackers is a Computer Science club at Diablo Valley College that focuses on practicing software engineering interview questions, solving competitive programming problems, and building cool projects! We were founded in Fall of 2019 and have over 30 members. All DVC students, no matter your skill level, are welcome to join! We will begin meeting
- again in Spring 2021.</div>
+                    <div id="description">DVHackers is a Computer Science club at Diablo Valley College that focuses on practicing software engineering interview questions, solving competitive programming problems, and building cool projects! We were founded in Fall of 2019 and have over 30 members. All DVC students, no matter your skill level, are welcome to join! Meetings restart
+in Fall 2022.</div>
                     <img src={require("./img/group-picture.jpeg")}/>
                 </div>
             </div>
