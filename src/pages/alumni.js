a9ba62899996ec08de405052f8e1a42e4c687551
Yahya <54677127+yahya-s9@users.noreply.github.com>
2021-08-09 15:29:45 -0700

    Update alumni.js
    added Previous officers to Alumni section

--- src/pages/alumni.js
+++ src/pages/alumni.js
@@ -10,8 +10,21 @@ import umidImg from './img/Muradli.jpg'
 import sebastianImg from './img/Lopez.jpg'
 import omarImg from './img/Ahmadieh.jpg'
 import avatarImg from './img/avatar.png'
+import mardanImg from './img/Mahmut.png'
+import daehoonImg from './img/Kwack.png'
+import daniyalImg from './img/Dianati.png'
+import olenaImg from './img/Buriakova.png'
+import rishiImg from './img/Thakkar.png'
+import wonjaeImg from './img/Lee.png'
 
-var members = [{ "name": "Can Koz", "email": "canxkoz@gmail.com", "github": "https://github.com/canxkoz", "linkedin": "https://www.linkedin.com/in/canxkoz/", "img": canImg },
+var members = [
+{ "name": "Wonjae Lee", "email": "wonjaelee99@gmail.com", "github": "http://github.com/leewjae","linkedin": "https://www.linkedin.com/in/wonjae-lee-193b591b1/", "img": wonjaeImg },
+{ "name": "Olena Buriakova","email": "oburiakova805@insite.4cd.edu", "github": "http://github.com/olenaaa", "linkedin": "https://www.linkedin.com/in/olena-buriakova-65a331194/", "img": olenaImg },
+{ "name": "Daehoon Kwack","email": "daehoonkwack@gmail.com", "github": "http://github.com/dkwack4059", "linkedin": "https://www.linkedin.com/in/daehoon-kwack-00b591193/","img": daehoonImg },
+{ "name": "Rishi Thakkar","email": "rishi.r.thakkar@gmail.com", "linkedin": "https://www.linkedin.com/in/rishi-thakkar-8a95a6157/", "img": rishiImg },
+{ "name": "Daniyal Dianati","email": "ddianati001@insite.4cd.edu", "linkedin": "https://www.linkedin.com/in/daniel-dianati-72bb18154", "img": daniyalImg },
+{ "name": "Mardan Mahmut", "email": "mardanmahmut@gmail.com", "github": "http://github.com/Mardan21", "linkedin": "https://www.linkedin.com/in/mardan-mahmut-840244173/", "img": mardanImg },
+{ "name": "Can Koz", "email": "canxkoz@gmail.com", "github": "https://github.com/canxkoz", "linkedin": "https://www.linkedin.com/in/canxkoz/", "img": canImg },
 { "name": "Andrew Che", "email": "andrew.che0122@gmail.com", "github": "https://github.com/drewche", "linkedin": "https://www.linkedin.com/in/andrew-che-314a55172/", "img": andrewImg },
 { "name": "Jacob Hanson", "email": "jhanreg11@gmail.com", "github": "https://github.com/jhanreg11", "linkedin": "https://www.linkedin.com/in/jacob-h-1746a7117/", "img": jacobImg },
 { "name": "Jake Nations", "email": "jakenations4@gmail.com", "github": "https://github.com/ClassicNations", "linkedin": "https://www.linkedin.com/in/jake-nations-5b9a53136/","img": jakeImg },
@@ -38,8 +51,10 @@ export default class Team extends React.Component {
     return (
       <div className="section" id='alumni'>
           <Row className='title'>Alumni</Row>
-          <Row className='row1'>{MemberList(members.slice(0,4))}</Row>
-          <Row className='row2'>{MemberList(members.slice(4,7))}</Row>
+          <Row className='row1'>{MemberList(members.slice(0,3))}</Row>
+          <Row className='row2'>{MemberList(members.slice(3,6))}</Row>
+          <Row className='row2'>{MemberList(members.slice(6,9))}</Row>
+          <Row className='row2'>{MemberList(members.slice(9,13))}</Row>
       </div>
     )
   }
