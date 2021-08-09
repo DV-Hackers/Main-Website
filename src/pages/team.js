55af915d517aa7a1c56bfeee1452b72e60506ad8
Yahya <54677127+yahya-s9@users.noreply.github.com>
2021-08-09 15:24:52 -0700

    Update team.js
    Added new officers

--- src/pages/team.js
+++ src/pages/team.js
@@ -2,19 +2,16 @@ import React from 'react';
 import './css/team.css'
 import Card from './components/card'
 import { Container, Jumbotron, Row } from 'reactstrap'
-import daehoonImg from './img/Kwack.png'
-import daniyalImg from './img/Dianati.png'
-import olenaImg from './img/Buriakova.png'
-import mardanImg from './img/Mahmut.png'
-import rishiImg from './img/Thakkar.png'
-import wonjaeImg from './img/Lee.png'
 
-var members = [{ "name": "Mardan Mahmut", "position": "President", "email": "mardanmahmut@gmail.com", "github": "http://github.com/Mardan21", "linkedin": "https://www.linkedin.com/in/mardan-mahmut-840244173/", "img": mardanImg },
-{ "name": "Wonjae Lee", "position": "Vice President", "email": "wonjaelee99@gmail.com", "github": "http://github.com/leewjae", "img": wonjaeImg },
-{ "name": "Olena Buriakova", "position": "ICC Rep", "email": "oburiakova805@insite.4cd.edu", "github": "http://github.com/olenaaa", "linkedin": "https://www.linkedin.com/in/olena-buriakova-65a331194/", "img": olenaImg },
-{ "name": "Daehoon Kwack", "position": "Treasurer", "email": "daehoonkwack@gmail.com", "github": "http://github.com/dkwack4059", "linkedin": "https://www.linkedin.com/in/daehoon-kwack-00b591193/","img": daehoonImg },
-{ "name": "Rishi Thakkar", "position": "Secretary", "email": "rishi.r.thakkar@gmail.com", "linkedin": "https://www.linkedin.com/in/rishi-thakkar-8a95a6157/", "img": rishiImg },
-{ "name": "Daniyal Dianati", "position": "Officer", "email": "ddianati001@insite.4cd.edu", "linkedin": "https://www.linkedin.com/in/daniel-dianati-72bb18154", "img": daniyalImg },
+
+import youssefImg from './img/youssef.jpg'
+import yahyaImg2 from './img/Yahya2.jpg'
+import aliImg from './img/Ali.JPG
+
+var members = [
+{ "name": "Youssef Qteishat", "position": "President", "email": "youssef.qt1@gmail.com", "github": "https://github.com/youssef-qteishat", "linkedin": "https://www.linkedin.com/in/youssef-qteishat-34b05a203", "img": youssefImg },
+{ "name": "Ali Tout", "position": "Vice President", "email": "at4882@gmail.com", "github": "https://github.com/Creater2036", "linkedin": " https://www.linkedin.com/in/ali-tout-0067961ba", "img": aliImg },
+{ "name": "Yahya Qʈeishat", "position": "Secratary", "email": "yahya.qteishat@gmail.com", "github": "https://github.com/yahya-s9","img": yahyaImg2 },
 ]
 
 function MemberList(members) {
